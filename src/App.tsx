ac2ba91aa143c2a96b1565451d4b49314e261433
Adam Martin <adam.d.martin1986@gmail.com>
2020-09-14 19:56:44 -0400

    merging code

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,20 @@ import Weather from './Weather';
 
 
 function App() {
-  
+
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
-  useEffect(() =>{
+  useEffect(() => {
     setLatitude(39.8475)
     setLongitude(86.2057)
   }, []);
 
-  const weatherURL=`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=089d47cbdc7d06d1a6c02d626f883233`
+  const weatherURL = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=089d47cbdc7d06d1a6c02d626f883233`
   return (
- 
+
     <div>
-      <Weather weatherURL={weatherURL} longitude={longitude} latitude={latitude}/>
-     
+      <Weather weatherURL={weatherURL} longitude={longitude} latitude={latitude} />
+
     </div>
   );
 }
